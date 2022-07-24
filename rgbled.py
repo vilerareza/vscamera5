e0e585bc23bef2813a48fb3e2c4ab28bc70dca13
Reza Vilera <vilera_reza99@yahoo.com>
2022-07-24 10:29:43 +0700

    create rgbled.py

--- rgbled.py
+++ rgbled.py
@@ -7,7 +7,7 @@ class RGBLed():
 
     def __init__(self) -> None:
         self.color = neopixel.NeoPixel(self.pin, 1)
-        self.set_color(0, 0, 0)
+        self.set_color((0, 0, 0))
 
     def set_color(self, color = (0, 0, 0)):
         self.color[0] = color
