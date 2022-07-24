93a2ba4b6fab3a070bdd73927a7aba469cbde804
Reza Vilera <vilera_reza99@yahoo.com>
2022-07-24 10:23:09 +0700

    create rgbled.py

---
+++ rgbled.py
@@ -0,0 +1,14 @@
+import board
+import neopixel
+
+class RGBLed():
+
+    def __init__(self, pin = "D10") -> None:
+        self.color = neopixel.NeoPixel(f'board.{pin}', 1)
+        self.set_color(0, 0, 0)
+
+    def set_color(self, color = (0, 0, 0)):
+        self.color[0] = color
+
+status_led = RGBLed()
+status_led.set_colot(0,255,0)
