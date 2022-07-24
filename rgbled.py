2c870f276d13f270f44f81fc728e7d1e7a36ee5a
Reza Vilera <vilera_reza99@yahoo.com>
2022-07-24 11:04:56 +0700

    create rgbled.py

--- rgbled.py
+++ rgbled.py
@@ -7,18 +7,18 @@ class RGBLed():
     pin = board.D10
 
     def __init__(self) -> None:
-        self.color = neopixel.NeoPixel(self.pin, 1, brightness = 0.1, auto_write = True, pixel_order = neopixel.RGBW)
+        self.color = neopixel.NeoPixel(self.pin, 1, brightness = 0.1, auto_write = False, pixel_order = neopixel.RGBW)
         #self.set_brightness()
-        self.set_color((0, 0, 0))
+        self.set_color((0, 0, 0, 0))
 
     def set_brightness(self, brightness = 0.1):
         self.color.brightness = brightness
 
-    def set_color(self, color = (0, 0, 0)):
+    def set_color(self, color = (0, 0, 0, 0)):
         self.color[0] = color
 
 status_led = RGBLed()
-status_led.set_color((0,0,0,0))
+status_led.set_color((0,255,0,0))
 time.sleep(5)
 status_led.color.fill((0,0,0,0))
 time.sleep(5)
